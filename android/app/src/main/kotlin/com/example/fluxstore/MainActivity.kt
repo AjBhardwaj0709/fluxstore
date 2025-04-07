e64379c6a2881c3e32e16f8ad010c8f8c74af6ed
Ajay Bhardwaj <ajaybhardwaj28102002@gmail.com>
2025-04-07 20:06:52 +0530

    Remove google-services.json and added to .gitignore

--- android/app/src/main/kotlin/com/example/fluxstore/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fluxstore/MainActivity.kt
@@ -2,4 +2,20 @@ package com.example.fluxstore
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity(){
+      private val CHANNEL = "screenshot_channel"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "allowScreenshots") {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+                result.success("Screenshots allowed")
+            } else {
+                result.notImplemented()
+            }
+        }
+}
+}
